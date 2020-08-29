e646d1d38e8e35425764674939a1bf716810439b
Jared Beach <jaredbeachdesign@gmail.com>
2020-08-29 15:47:07 -0500

    init

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello jared");
+
+        int myFirstNumber = 5;
+        System.out.println("5 / 2 = " + (myFirstNumber / 2));
+    }
+}

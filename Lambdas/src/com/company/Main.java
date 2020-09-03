a84380d82dae7b8433d9ea6ae9dfbdd06cc154a1
Jared Beach <jaredbeachdesign@gmail.com>
2020-09-03 18:58:10 -0500

    lambda for interfaces with one method

--- Lambdas/src/com/company/Main.java
+++ Lambdas/src/com/company/Main.java
@@ -32,6 +32,21 @@ public class Main {
         for(Employee employee: employees) {
             System.out.println(employee.getName());
         }
+
+        String test1 = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, "it ", "works");
+        System.out.println(test1);
+
+        String x = doStringStuff((String s1, String s2) -> s1.toUpperCase() + s2.toUpperCase(), employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(x);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -59,4 +74,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }

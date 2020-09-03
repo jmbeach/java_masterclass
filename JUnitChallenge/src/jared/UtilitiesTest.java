302aa4eefa10aebf57699fddfa49e31d6329bea4
Jared Beach <jaredbeachdesign@gmail.com>
2020-09-02 20:50:30 -0500

    testing stuff

---
+++ JUnitChallenge/src/jared/UtilitiesTest.java
@@ -0,0 +1,26 @@
+package jared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void everyNthChar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePairs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void converter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nullIfOddLength() {
+    }
+}

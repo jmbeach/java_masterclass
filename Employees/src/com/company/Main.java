f0095356963dcb4332aa358e72188a7db065d29c
Jared Beach <jaredbeachdesign@gmail.com>
2020-09-03 20:29:46 -0500

    java.util.function

--- Employees/src/com/company/Main.java
+++ Employees/src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class Main {
 
@@ -22,14 +23,73 @@ public class Main {
         employees.add(red);
         employees.add(charming);
 
-        printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employees, "\nEmployees 30 and younger", employee -> employee.getAge() <= 30);
-        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
-            @Override
-            public boolean test(Employee employee) {
-                return employee.getAge() < 25;
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(" ") + 1);
+        };
+
+//        employees.forEach(employee -> System.out.println(getLastName.apply(employee)));
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+//        employees.forEach(employee -> System.out.println(getFirstName.apply(employee)));
+
+        Random r1 = new Random();
+        for (Employee employee: employees) {
+            if (r1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
             }
-        });
+            else {
+                System.out.println(getAName(getLastName, employee));
+            }
+        }
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+        Function chainedFunction = upperCase.andThen(firstName);
+        System.out.println(chainedFunction.apply(employees.get(0)));
+
+        BiFunction<String, Employee, String> concatAge = (String name, Employee employee) -> {
+            return name.concat(" " + employee.getAge());
+        };
+
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println(concatAge.apply(upperName, employees.get(0)));
+
+        IntUnaryOperator incBy5 = i -> i + 5;
+        System.out.println(incBy5.applyAsInt(10));
+
+        Consumer<String> c1 = s -> s.toUpperCase();
+        Consumer<String> c2 = s -> System.out.println(s);
+        c1.andThen(c2).accept("Hello world");
+//        printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
+//        printEmployeesByAge(employees, "\nEmployees 30 and younger", employee -> employee.getAge() <= 30);
+//        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+//            @Override
+//            public boolean test(Employee employee) {
+//                return employee.getAge() < 25;
+//            }
+//        });
+
+//        IntPredicate greaterThan15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//        System.out.println("10 > 15 = " + greaterThan15.test(10));
+//        int a = 20;
+//        System.out.println(greaterThan15.test(a + 5));
+//        System.out.println(greaterThan15.and(lessThan100).test(50));
+//        System.out.println(greaterThan15.or(lessThan100).test(15));
+
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(randomSupplier.get());
+//        }
+
+//        employees.forEach(employee -> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(" ") + 1);
+//            System.out.println("Last name is: " + lastName);
+//        });
     }
 
     private static void printEmployeesByAge(List<Employee> employees,
@@ -42,5 +102,9 @@ public class Main {
                 System.out.println(employee.getName());
             }
         }
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
     }
 }
